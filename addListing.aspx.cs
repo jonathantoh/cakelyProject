0e00b1add47879cc672e6c405aac12801b33b80e
Jonathan <jonathantohjj@gmail.com>
2019-07-08 11:52:56 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/jonathantoh/cakelyProject""
    This reverts commit 168614e3019d5c40b2191d59ef688430b7ef4542.

---
+++ addListing.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class addListing : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_Insert_Click(object sender, EventArgs e)
+    {
+        int result = 0;
+        string image = "";
+        string category = "";
+
+        decimal budget = decimal.Parse(budgetID.Text);
+
+        if (FilePhoto.HasFile == true)
+        {
+            image = "Images\\" + FilePhoto.FileName;
+        }
+
+
+        string custEmail = Session["user"].ToString();
+
+        Listings prod = new Listings(); //tb_ProductCat can change to ddl_ProdCat
+
+        result = prod.ListingInsert(listingTitleID.Text, descID.Text, (FilePhoto.FileName).ToString(), budget, custEmail);
+
+        if (result > 0)
+        {
+            string saveimg = Server.MapPath("") + "\\" + image;
+            FilePhoto.SaveAs(saveimg);
+
+            //loadProductInfo();
+            //loadProduct();
+            //clear1();
+
+            Response.Write("<script> alert('Insert successful');</script>");
+        }
+        else { Response.Write("<script> alert('Insert NOT  succesful');</script>"); }
+    }
+}
---
+++ login.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Security;
+
+public partial class login : System.Web.UI.Page
+{
+
+    string _connStr = ConfigurationManager.ConnectionStrings["cakelyDB"].ConnectionString.ToString();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["user"] != null)
+        {
+            Response.Redirect("~/account.aspx");
+        }
+    }
+
+    protected void btn_submit_Click(object sender, EventArgs e)
+    {
+
+        int result = 1;
+        //Customer cust = new Customer(txtusername.Text, txtpassword.Text);
+        //result = cust.UserLogin();
+
+        if (result > 0)
+        {
+            Session["user"] = txtusername.Text;
+            Response.Redirect("~/listingCatalog.aspx");
+        }
+        else
+        {
+            Response.Write("<script>alert('Login NOT successful');</script>");
+        }
+        //SqlConnection conn = new SqlConnection(_connStr);
+        //conn.Open();
+        //string query = "SELECT count(*) FROM customer where custEmail='" + txtusername.Text + "' and custPassword='" + txtpassword.Text + "'";
+
+        //SqlCommand cmd = new SqlCommand(query, conn);
+        //string output = cmd.ExecuteScalar().ToString();
+
+        //if(output == "1")
+        //{
+        //    //creating a session
+        //    Session["user"] = txtusername.Text;
+        //    Response.Redirect("~/account.aspx");
+        //}
+        //else
+        //{
+        //    Response.Write("Login Failed");
+        //}
+
+
+
+
+    }
+}
